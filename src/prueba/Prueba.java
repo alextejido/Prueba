bfceca0248296c006eaf08b4230ecca92cbb022a
atejidosolla <atejidosolla@atanasoff29.danielcastelao.org>
2016-10-10 09:40:52 +0200

    inicio projecto

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,14 @@
+
+package prueba;
+
+
+public class Prueba {
+
+    
+    public static void main(String[] args) {
+        int valor = 3;
+        System.out.println("Hola mundo"+valor);
+        System.out.println("valor");
+    }
+    
+}
